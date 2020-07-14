f257a8d11266fd74cafad3105eb4978c16b2b55e
loay <loayezzat16@gmail.com>
2020-07-14 09:31:56 +0200

    First Attempt, Over Fitting

--- models.py
+++ models.py
@@ -20,7 +20,20 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv1 = nn.Conv2d(1, 32, 3 , padding = 1 )
+        self.batch1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding = 1 )
+        self.batch2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding = 1 )
+        self.batch3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 256, 3, padding = 1 )
+        self.batch4 = nn.BatchNorm2d(256)
+        self.pool =  nn.MaxPool2d(2, 2)    
+        self.dropout = nn.Dropout(0.3)
+        
+        self.fc1 = nn.Linear(256 *14*14,1024)
+        self.fc2 = nn.Linear(1024,136)
+        
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -30,8 +43,13 @@ class Net(nn.Module):
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
-        
-        
+        batch_size = x.shape[0]
+        x = self.pool(self.batch1(F.relu(self.conv1(x))))
+        x = self.pool(self.batch2(F.relu(self.conv2(x))))
+        x = self.pool(self.batch3(F.relu(self.conv3(x))))
+        x = self.pool(self.batch4(F.relu(self.conv4(x))))
+        x = x.view (batch_size, -1)
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.fc2(x)
         # a modified x, having gone through all the layers of your model, should be returned
         return x
